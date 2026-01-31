048028cc3b4f37d7f1483ad72c36000f701499f8
Ghost Hacker <ghost.hacker.uk@gmail.com>
2026-01-31 10:45:05 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_e0cXP77xLFNoFP3Ywk3P-Qhyba5E",
+  apiKey: "AIzaSyD_eOcX7P77xLFnOPf3yWk3P-Qyhbyab5E",
   authDomain: "phone-guard-user-new.firebaseapp.com",
   projectId: "phone-guard-user-new",
-  storageBucket: "phone-guard-user-new.appspot.com",
+  storageBucket: "phone-guard-user-new.firebasestorage.app",
   messagingSenderId: "764033397081",
-  appId: "1:764033397081:web:182fdc27cfc663e36c5c48"
+  appId: "1:764033397081:web:182fd2c7cfc663e36c5c48"
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
